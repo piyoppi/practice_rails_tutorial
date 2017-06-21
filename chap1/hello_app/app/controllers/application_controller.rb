4beda26f6a7b01f26911f7d9325d4ec4b4f8db29
Shohei Ogata <shohei.ogata@pepabo.com>
2017-06-21 14:24:24 +0900

    goodbye worldコントローラを追加してrouterを書き換えた

--- chap1/hello_app/app/controllers/application_controller.rb
+++ chap1/hello_app/app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, world"
+    render html: "!¡hola, mundo!"
+  end
+
+  def goodbye
+    render html: "goodbye, world!"
   end
 end
